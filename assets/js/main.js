de7585f99d43ef469be5d0bbd055447fc982b818
Quangduc2002 <89673332+Quangduc2002@users.noreply.github.com>
2021-11-15 17:45:40 +0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function domouseout() {
+    document.getElementById("Quicklinks1").style.display= "none";
+}
+
+function dosomething() {
+    document.getElementById("Quicklinks1").style.display = "block";
+}
+
+function domouseout1() {
+    document.getElementById("Quicklinks2").style.display= "none";
+}
+
+function dosomething1() {
+    document.getElementById("Quicklinks2").style.display = "block";
+}
+
+
+function domouseout2() {
+    document.getElementById("Quicklinks3").style.display= "none";
+}
+
+function dosomething2() {
+    document.getElementById("Quicklinks3").style.display = "block";
+}
+
+
